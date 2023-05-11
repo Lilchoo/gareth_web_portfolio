2f4dc6bec2d5e819a2b65da1e9ef6c0d6d3f0a39
Gareth Ng <ngys.gareth@gmail.com>
2023-05-10 17:36:25 -0700

    First commit

---
+++ src/assets/index.js
@@ -0,0 +1,65 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import learner from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import aws from "./tech/aws.png"
+import css from "./tech/css.png";
+import firebase from "./tech/firebase.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import androidstudio from "./tech/androidstudio.png";
+import tailwind from "./tech/tailwind.png";
+import python from "./tech/python.png";
+import java from "./tech/java.png";
+
+
+import td from "./company/td.png";
+import rbc from "./company/rbc.png";
+import yvr from "./company/yvr.png"
+
+import pokedex from "./pokedex.png";
+import wecollaba from "./wecollaba.png"
+import wecollabw from "./wecollabw.png"
+import boids from "./boids.png"
+
+export {
+  logo,
+  backend,
+  learner,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  aws,
+  css,
+  firebase,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  androidstudio,
+  tailwind,
+  python,
+  java,
+  td,
+  rbc,
+  yvr,
+  pokedex,
+  wecollaba,
+  wecollabw,
+  boids,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,252 @@
+import {
+    mobile,
+    backend,
+    learner,
+    web,
+    javascript,
+    python,
+    java,
+    html,
+    css,
+    reactjs,
+    androidstudio,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    firebase,
+    aws,
+    td,
+    yvr,
+    rbc,
+    wecollaba,
+    wecollabw,
+    pokedex,
+    boids,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Android Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Lifelong Learner",
+      icon: learner,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "Python",
+      icon: python,
+    },
+    {
+      name: "Java",
+      icon: java,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Android Studio",
+      icon: androidstudio,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Git",
+      icon: git,
+    },
+    {
+      name: "Figma",
+      icon: figma,
+    },
+    {
+      name: "Firebase",
+      icon: firebase,
+    },
+    {
+      name: "AWS",
+      icon: aws,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Cloud Solution Architecture - Volunteer",
+      company_name: "YVR Vancovuer Airport",
+      icon: yvr,
+      iconBg: "#E6DEDD",
+      date: "April 2023 - Dec 2023",
+      points: [
+        "Designed and implemented a robust cloud infrastructure on AWS to facilitate bidirectional communication between buoys deployed throughout YVR airport and the dashboard team.",
+        "Established seamless data transmission from buoys to the dashboard and vice versa, enabling system calibration and real-time monitoring of buoy data.",
+        "Configured a scalable database system on AWS to efficiently store and manage data received from the buoys, ensuring availability for future analytics and reporting.",
+        "Collaborated with service providers and leveraged AWS services to optimize performance, security, and cost-effectiveness of the cloud-based solution for YVR.",
+      ],
+    },
+    {
+      title: "Financial Advisor",
+      company_name: "Toronto Dominion Bank",
+      icon: td,
+      iconBg: "#383E56",
+      date: "May 2021 - Jan 2022",
+      points: [
+        "Proactively guide clients in developing personalized investment strategies, considering their financial goals, risk tolerance, and time horizon.",
+        "Offer comprehensive financial planning services, including retirement planning, tax optimization, and estate planning, to help clients achieve long-term financial success.",
+        "Provide expert guidance on mortgage options, analyzing financial data, and assisting clients in securing the most favorable mortgage terms and rates.",
+        "Foster strong client relationships through regular communication, financial education, and continuous monitoring of investment portfolios and mortgage refinancing opportunities.",
+      ],
+    },
+    {
+      title: "Banking Advisor",
+      company_name: "Royal Bank of Canada",
+      icon: rbc,
+      iconBg: "#E6DEDD",
+      date: "May 2019 - May 2021",
+      points: [
+        "Act as a client-focused advisor, providing comprehensive banking solutions and addressing everyday banking, credit, and investment needs..",
+        "Utilize expert problem-solving skills to deliver exceptional client experiences, leveraging technology and digital banking platforms.",
+        "Proactively engage with clients, uncovering their unique banking requirements, and identifying sales and referral opportunities.",
+        "Collaborate with internal partners to maximize business opportunities, while maintaining strong client relationships and driving client satisfaction and loyalty.",
+      ],
+    },
+  ];
+
+  const testimonials = [
+    {
+      testimonial:
+        "Achieved 1st place in the YVR Inauguration Hackathon 2023 with \"Crow's Nest\", an AI-based project that effectively addresses real-world challenges at YVR, contributing to their NET ZERO 2030 goal.",
+      name: "YVR Airport"
+    },
+    {
+      testimonial:
+        "Received an Award from RBC as the Top Sales Performer in East Vancouver, Burnaby & New Westminster Markets for Q3 2020, demonstrating exceptional sales achievements and contributions.",
+      name: "Royal Bank of Canada"
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Pokedex",
+      description:
+        "Developed a Pokedex utilizing Python, RESTful APIs, and design patterns including Chain of Responsiblity to ensure organized and indsutry-standard code.",
+      tags: [
+        {
+          name: "python",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "RESTful-APIs",
+          color: "green-text-gradient",
+        },
+        {
+          name: "oop-design-patterns",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: pokedex,
+      source_code_link: "https://github.com/Valeraii/2800-202205-BBY2.git",
+    },
+    {
+      name: "weCollab",
+      description:
+        "Web application that enables users to browse and purchase merchandise and includes features such as user authentication, cart management, and payment processing.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: wecollabw,
+      source_code_link: "https://github.com/Lilchoo/cloud3962-weCollab",
+    },
+    {
+      name: "weCollab-mobile",
+      description:
+        "A mobile application of weCollab.",
+      tags: [
+        {
+          name: "androidstudio",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "kotlin",
+          color: "green-text-gradient",
+        },
+        {
+          name: "firebase",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: wecollaba,
+      source_code_link: "https://github.com/Lilchoo/weCollab-android",
+    },
+    {
+      name: "Boids",
+      description:
+        "A Java application inspired by Boids, an artificial life program created by Craig Reynolds in 1986. It simulates the flocking behaviour of birds, offering as fascinating glimpse into collective movement patterns.",
+      tags: [
+        {
+          name: "java",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "oop",
+          color: "green-text-gradient",
+        },
+      ],
+      image: boids,
+      source_code_link: "https://github.com/Lilchoo/boids-java",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
